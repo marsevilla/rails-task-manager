66b75f40a1de086911585a829f04a53cd770935d
Maria del Mar Sevilla Escobar <sevillamariadelmar@gmail.com>
2024-04-23 13:24:17 +0200

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # READ ALL
+  get "/tasks", to: "tasks#index", as: :tasks
+
+  #CREATE ONE
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  post "/tasks", to: "tasks#create"
+
+  # READ ONE
+  get "/tasks/:id", to: "tasks#show", as: :task
+
+  # UPDATE
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "/tasks/:id", to: "tasks#update"
+
+  # DELETE
+  delete "/tasks/:id", to: "tasks#destroy"
 end
